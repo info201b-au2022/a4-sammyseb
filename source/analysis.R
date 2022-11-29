22042314137d1508e68027adecf3b57c2eb4b949
Samuel Shumye <samuelshumye@Samuels-MacBook-Air-2.local>
2022-11-28 22:06:03 -0800

    update

--- source/analysis.R
+++ source/analysis.R
@@ -6,6 +6,7 @@ library("mapproj")
 library("patchwork")
 # The functions might be useful for A4
 source("../source/a4-helpers.R")
+
 jail_info <- read.csv ("https://raw.githubusercontent.com/vera-institute/incarceration-trends/master/incarceration_trends.csv")
 
 ## Test queries ----
@@ -31,10 +32,10 @@ test_query2 <- function(num=6) {
 ## Section 3  ---- 
 get_year_jail_pop <- function() {
   view_jail <- jail_info %>% 
-    select(year, total_jail_pop, state)%>% 
+    select(year, total_jail_pop)%>% 
     drop_na() %>% 
     group_by(year) %>% 
-    summarize(jail_all = sum(total_jail_pop, na.rm = TRUE))
+    summarize(jail_all = sum(total_jail_pop)) %>% 
   return(view_jail)
 } 
 
@@ -48,16 +49,6 @@ plot_jail_pop_for_us <- function() {
 }  
 results <- plot_jail_pop_for_us () 
 
-
-
-
-#   p <- plot (get_year_jail_pop) +
-# geom_line(mapping = aes(x = year, y= jail_all, color = "red")) +
-# scale_color_manual(name = "race",
-#                    values = c("red"),
-#                    labels = c("black")) +
-# labs(x= "year", y= "Amount of people in Jail") +                 
-# ggtitle("TimComparison Chart of incarceration through the years")
 #----------------------------------------------------------------------------#
 # Growth of the U.S. Prison Population
 # Your functions might go here ... <todo:  update comment>
@@ -79,43 +70,27 @@ results <- plot_jail_pop_for_us ()
 # Growth of Prison Population by State 
 # Your functions might go here ... <todo:  update comment>
 # See Canvas
-
+states <- c("WA", "OR", "CA")
   get_jail_pop_by_states <- function(states) { 
+    states <- c("WA", "OR", "CA")
     Target <- jail_info %>%
+      filter(state %in% states)%>%
       select(total_jail_pop, year, state) %>%
-      drop_na() %>%
-      filter(state %in% states)
-    
+      drop_na() %>% 
+
+
     return  (data.frame(Target))
   }
-    # Results_jail <- jail_info %>%
-    #   select(total_jail_pop, year, state) %>%
-    #   group_by(year) %>%
-    #   filter(state == "WA") %>%
-    #   summarize(total_WA = sum(total_jail_pop, na.rm = TRUE))
-    # return (Results_jail)
-    # 
-    # get_jail_pop_by_states <- function(states) {
-    #    jail_info %>% 
-    #     select(total_jail_pop, state)%>% 
-    #     drop_na() %>% 
-    #     group_by(state) %>% 
-    #     summarize(jail_all = sum(total_jail_pop))
-    #   return(jail_info)
-    
-
-# }
-
-
-plot_jail_pop_by_states(c("WA", "OR", "CA")) <- function() {
-  get_jail <- get_jail_pop_by_states
-  p <- ggplot(plot =  get_jail) +
-  geom_line(mapping = aes(x = year, y=state)) +
+  
+plot_jail_pop_by_states <- function (states) {
+  get_jail <- get_jail_pop_by_states (states)
+  p <- ggplot(data = get_jail) +
+  geom_col(mapping = aes(x = state , y=total_jail_pop)) +
   labs(x= "year", y = "Amount of people in jail") +
   ggtitle(" Growth of prison population in Washington from 1970-2018")
   return(p)
 }
-  
+  task <- plot_jail_pop_by_states()
 #----------------------------------------------------------------------------#
 
 ## Section 5  ---- 
@@ -126,16 +101,15 @@ plot_jail_pop_by_states(c("WA", "OR", "CA")) <- function() {
 
 inequality_data <-  function () {
  results <-  jail_info %>% 
-   select(year, blach_jail_pop, white_jail_pop)
     group_by(year) %>% 
     summarize(black_all = sum(black_jail_pop, na.rm = TRUE), white_all = sum(white_jail_pop, na.rm = TRUE)) %>% 
     return(results)
 }
 
   
-over_Time <- function (){
-  ineq_info <- inequality_data
- p <-  ggplot(plot =  ineq_info) +
+Over_time <- function (){
+  injust_rate <- inequality_data()
+ p <-  ggplot(data =  injust_rate) +
     geom_line(mapping = aes(x = year, y= black_all, color = "red")) +
     geom_line(mapping = aes(x = year, y= white_all, color = "blue")) +
     scale_color_manual(name = "race",
@@ -146,6 +120,7 @@ over_Time <- function (){
     return(p)
 }
 
+determine <- Over_time()
 
 #----------------------------------------------------------------------------#
 
@@ -154,16 +129,16 @@ over_Time <- function (){
 # <a map shows potential patterns of inequality that vary geographically>
 # Your functions might go here ... <todo:  update comment>
 # See Canvas
-AL_Map <-  jail_info %>% 
-  filter(year == max(year)) 
+TX_Map <- proj %>% 
+  filter(year == max(year))
 
 county_shapes <- map_data("county") %>%
   unite(polyname, region, subregion, sep = ",") %>% 
   left_join(county.fips, by = "polyname")
 
-map_data <- county_shapes %>% 
-  left_join(AL_Map, by ="fips") %>% 
-  filter(state == "TX")
+# map_data <- county_shapes %>% 
+#  left_join(TX_Map, by ="fips") %>% 
+#   filter(state == "TX")
 
 #clean map
 
@@ -180,19 +155,15 @@ blank_theme <- theme_bw() +
   )
 
 #create map
-cases_Map <- 
-  ggplot(map_data) +
-    geom_polygon(
-      mapping = aes(x= long, y= lat, group = group, fill = black_jail_pop),
-      color="gray", size = 0.3
-    ) +
-    coord_map() +
-    scale_fill_continuous(limits = c(0, max(map_data$black_jail_pop)), na.value= "white", low = "yellow", high = "red") +
-    blank_theme +
-    ggtitle("Black Jail Population in Alabama")
-
-  
-
+cases_Map <- ggplot(map_data) +
+  geom_polygon(
+    mapping = aes(x= long, y= lat, group = group, fill = black_jail_pop),
+    color="gray", size = 0.3
+  ) +
+  coord_map() +
+  scale_fill_continuous(limits = c(0, max(map_data$black_jail_pop_rate)), na.value= "white", low = "yellow", high = "red") +
+  blank_theme +
+  ggtitle("Black Jail Population in Alabama")
 
 #----------------------------------------------------------------------------#
 
